eb8932b12ae55000f0f2c351c71d5e4155d72acf
Fikriaz <fikri.achmadfauzi77@gmail.com>
2025-07-09 23:15:11 +0700

    Menambahakn testing invalid edit

--- tests/cypress/integration/users/EditUsers.cy.js
+++ tests/cypress/integration/users/EditUsers.cy.js
@@ -66,3 +66,73 @@ beforeEach(() =>{
   })
 
 });
+
+describe("Invalid Edit Account", ()=>{
+
+    beforeEach(() =>{
+        cy.login();
+        cy.edit();
+    })
+
+  it('User cannot edit with empty field username', ()=> {
+    cy.contains('tr', 'user').within(()=> {
+      cy.contains('Edit').click();
+    })
+    cy.get('[data-id="name"]')
+    .clear()
+
+    cy.get('#email')
+    .clear()
+    .type('cindiana21@gmail.com')
+
+    //button submit
+    cy.contains('button','Submit').click()
+
+    //Assertion
+    cy.get('.invalid-feedback').should('be.visible');
+    cy.get('.invalid-feedback').contains('The name field is required.');
+  })
+  
+  it('User cannot edit with empty field email', ()=> {
+    cy.contains('tr', 'user').within(()=> {
+      cy.contains('Edit').click();
+    })
+    cy.get('[data-id="name"]')
+    .clear()
+    .type('cindiana')
+
+    cy.get('#email')
+    .clear()
+  
+
+    //button submit
+    cy.contains('button','Submit').click()
+
+    //Assertion
+    cy.get('.invalid-feedback').should('be.visible');
+    cy.get('.invalid-feedback').contains('The email field is required.');
+  })
+
+  it('User cannot edit with empty field username & email', ()=> {
+    cy.contains('tr', 'user').within(()=> {
+      cy.contains('Edit').click();
+    })
+    cy.get('[data-id="name"]')
+    .clear()
+   
+
+    cy.get('#email')
+    .clear()
+  
+
+    //button submit
+    cy.contains('button','Submit').click()
+
+    //Assertion
+    cy.get('.invalid-feedback').should('be.visible');
+    cy.get('.invalid-feedback').contains('The name field is required.');
+    cy.get('.invalid-feedback').contains('The email field is required.');
+
+  })
+
+})
