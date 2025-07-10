e2885e2d01b3f0d493d49e74a88e92557852d26b
Fikriaz <fikri.achmadfauzi77@gmail.com>
2025-07-10 22:06:07 +0700

    Add invalid delete Value

--- tests/cypress/integration/users/DeleteUsers.cy.js
+++ tests/cypress/integration/users/DeleteUsers.cy.js
@@ -3,7 +3,7 @@ describe('Validate delete user account', () => {
     cy.login(); // auto login sebelum setiap test
   }); 
 
-  it('User can Create new Account', () => {
+  it('User can delete Account', () => {
      cy.create();
 
     //fill form
@@ -27,6 +27,41 @@ describe('Validate delete user account', () => {
     cy.get('p')
     .should('be.visible')
     .contains('User Deleted Successfully')
+
+  })
+
+});
+
+describe('Invalidate delete user account', () => {
+  beforeEach(() => {
+    cy.login(); // auto login sebelum setiap test
+  }); 
+
+  it('User can Cancel delete user account', () => {
+     cy.create();
+
+    //fill form
+    cy.get('[id="name"]').type('budi');
+    cy.get('[id="email"]').type('budi@gmail.com');
+    cy.get('[id="password"]').type('Asqweqwe');
+    cy.contains('Submit').click();
+    
+    //Assert
+    cy.get('p').should('be.visible');
+    cy.get('p').contains('Data Berhasil Ditambahkan');
+  });
+
+  it('User can cancle delete account', () => {
+    cy.get('[data-id=Delete]').click()
+    cy.contains('OK')
+    .should("be.visible")
+    .click()
+
+    //Assert
+    cy.get('.section-header')
+    .should('be.visible')
+    .contains('h1','User List')
+    
 
   })
 
